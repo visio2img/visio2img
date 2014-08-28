7de853c3dfb6f7c411e93bf4f92527d3b4e7c289
Yuki Yasuda <mathyassu@gmail.com>
2014-08-28 11:42:47 +0900

    add some exceptions and refactoring

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='visio2img',
-    version='0.3rc',
+    version='0.3.1rc',
     packages=['visio2img']
     )
--- visio2img/visio2img.py
+++ visio2img/visio2img.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 import win32com.client
+from pywintypes import com_error
 from win32com.client import constants
 from os import path, chdir, getcwd
 
-from sys import exit
+from sys import exit, stderr
 from optparse import OptionParser
 
 __all__ = ['export_img']
+
+class IllegalImageFormatException(TypeError):
+    """
+    This exception means Exceptions for Illegal Image Format.
+    """
 
 def get_dispatch_format(extension):
     return 'Visio.InvisibleApp' # vsd format
@@ -26,14 +32,31 @@ def export_img(in_filename, out_filename, page_num=None):
     """
     export as image format
     """
+    # to absolute path
+    in_filename = path.abspath(in_filename)
+    out_filename = path.abspath(out_filename)
+    
     # define filename without extension and extension variable
     in_filename_without_extension, in_extension = path.splitext(in_filename)
     out_filename_without_extension, out_extension = path.splitext(out_filename)
 
+    if in_extension not in ('.vsd'):
+        err_str = (
+                'Input filename is not llegal for visio file. \n' 
+                'This program is suppert only vsd extension.'
+                )
+        raise IllegalImageFormatException(err_str)
+
+    if out_extension not in ('.gif', '.jpg', '.jpeg', '.png'):
+                err_str = (
+                'Output filename is not llegal for visio file. \n' 
+                'This program is suppert gif, jpeg, png extension.'
+                )
+                raise IllegalImageFormatException(err_str)
+
     # if file is not found, exit from program
     if not path.exists(in_filename):
-        print('File Not Found')
-        exit()
+        raise FileNotFoundError('Input File is not found.')
 
     try:
         # make instance for visio
@@ -54,7 +77,8 @@ def export_img(in_filename, out_filename, page_num=None):
         # Export pages
         for page, page_name in zip(pages, page_names):
             page.Export(page_name)
-
+    except com_error as err:
+        raise IllegalImageFormatException('Output filename is not llegal for Image File.')
     finally:
         application.Quit()
 
@@ -75,11 +99,17 @@ if __name__ == '__main__':
     
     # if len(arguments) != 2, raise exception
     if len(argv) != 2:
-        print('Enter Only input_filename and output_filename')
+        stderr.write('Enter Only input_filename and output_filename')
         exit()
     
     # define input_filename and output_filename
-    in_filename = path.abspath(argv[0])
-    out_filename = path.abspath(argv[1])
-    
-    export_img(in_filename, out_filename, options.page)
+    in_filename = argv[0]
+    out_filename = argv[1]
+
+    try:
+        export_img(in_filename, out_filename, options.page)
+    except (FileNotFoundError, IllegalImageFormatException) as err:
+                # expected exception
+        print(str(err)) # print message
+    except Exception as err:
+        print(err.__traceback__)      # TraceBack Information
