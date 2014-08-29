b64bd1409bcd7625b6cb6b5623aec11da6589acf
Yuki Yasuda <mathyassu@gmail.com>
2014-08-29 09:47:09 +0900

    add some information(temporary)

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup
 
 setup(
+    # program info
     name='visio2img',
     version='0.3.2rc',
-    packages=['visio2img']
+    packages=['visio2img'],
+    description='module or software for translation from visio format to other general image format.',
+    url='https://github.com/yassu/Visio2Img',
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'License :: Freeware',
+            'License :: OSI Approved :: MIT License'
+        ],
+    license=(
+        'Copyright (c) 2014 Yassu\n'
+        'Released Under the MIT license\n'
+        'https://github.com/yassu/Visio2Img\n'
+        ),
+
+    # author info
+    author='Yassu',
+    author_email='yassumath@gmail.com',
     )
