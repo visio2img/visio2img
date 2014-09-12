b9ae79d4584e06362751a5f62ea684e14d2a9eaf
Takeshi KOMIYA <i.tkomiya@gmail.com>
2014-09-12 19:55:28 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
 
 setup(
     name='visio2img',
-    version='1.0.0',
+    version='1.1.0',
     description='MS-Visio file (.vsd, .vsdx) to images converter',
     long_description=open('README.rst').read(),
     author='Yassu',
